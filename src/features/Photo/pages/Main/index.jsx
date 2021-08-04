d600ed27fb0c6f7856eee7083dc658c3be8d3fbc
HaTran <hachin2109@gmail.com>
2021-08-04 09:53:44 +0700

    add jsconfig

--- src/features/Photo/pages/Main/index.jsx
+++ src/features/Photo/pages/Main/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Banner from '../../../../components/Banner';
-import Images from '../../../../constants/images';
-import { Link } from 'react-router-dom';
-import { Container } from 'reactstrap';
+import Banner from "components/Banner";
+import Images from "constants/images";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container } from "reactstrap";
 
 MainPage.propTypes = {};
 
